ec3dbd8b573cbc8a9e44a5700ddb1eba7483345e
Louie Schoenknecht <luisesch97@gmail.com>
2021-05-05 14:40:59 -0700

    add header.js and header styling in index.css, attempt to install babel plugin to handle relative paths (wip)
    Co-authored-by: Ash Porter <porter.ashley13@gmail.com>

--- src/components/App.js
+++ src/components/App.js
@@ -1,20 +1,13 @@
+import React from 'react'
+import Header from './Header'
+
 function App() {
   return (
-    <div className="App component">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="app">
+        <Header />
+      </div>
+    </>
   );
 }
 
