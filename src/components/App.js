2b462fedea863ca2da28df7dc6a7fe195908df6c
Louie Schoenknecht <luisesch97@gmail.com>
2021-05-05 15:32:25 -0700

    add project.js with hardcoded data (for now) dquote> dquote> Co-authored-by: Ash Porter <porter.ashley13@gmail.com>

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import Header from './Header'
 import SignIn from "./SignIn"
+import ProjectList from './Projects/ProjectList'
 
 function App() {
   return (
@@ -13,6 +14,9 @@ function App() {
           <div className="column2">
             <SignIn />
           </div>
+        </div>
+        <div>
+          {/* <ProjectList/> */}
         </div>
       </div>
     </>
---
+++ src/components/Projects/Project.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Project() {
+  return (
+    <>
+      <h3>Super cool project</h3>
+      <p>Details about super cool project</p>
+    </>
+  );
+}
+
+export default Project;
